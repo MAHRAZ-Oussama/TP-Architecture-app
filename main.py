3aee258b6e31e5eabacacf2118708afd3f6ac652
Oussama Mahraz <oussama.mahraz1@gmail.com>
2026-03-25 12:33:00 +0100

    Q3: add rank_matter_1 method to SchoolClass

--- main.py
+++ main.py
@@ -13,8 +13,14 @@ class SchoolClass:
     def add_student(self, student):
         self.students.append(student)
 
+    def rank_matter_1(self):
+        sorted_students = sorted(self.students, key=lambda s: s.grade1, reverse=True)
+        for student in sorted_students:
+            print(student.name, student.grade1)
+
 
 school_class = SchoolClass()
 school_class.add_student(Student('J', 10, 12, 13))
 school_class.add_student(Student('A', 8, 2, 17))
 school_class.add_student(Student('V', 9, 14, 14))
+school_class.rank_matter_1()
